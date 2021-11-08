23d0ddde8d58a15383f503972250e8f31985c3ce
vrrpizzato <vrrpizzato@gmail.com>
2021-11-08 16:49:53 -0300

    generated context for accounts, and created entities and repositories folders

---
+++ lib/doctor_schedule/accounts/entities/user.ex
@@ -0,0 +1,23 @@
+defmodule DoctorSchedule.Accounts.Entities.User do
+  @moduledoc false
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :password_hash, :string
+    field :role, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :first_name, :last_name, :role, :password_hash])
+    |> validate_required([:email, :first_name, :last_name, :role, :password_hash])
+  end
+end
