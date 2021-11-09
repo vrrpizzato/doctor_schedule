b8ff910f5f93fb28f8fbaa4be5d7d36a0238b50a
vrrpizzato <vrrpizzato@gmail.com>
2021-11-09 14:32:01 -0300

    additional validations of changeset for users

--- priv/repo/migrations/20211108181201_create_users.exs
+++ priv/repo/migrations/20211108181201_create_users.exs
@@ -2,7 +2,7 @@ defmodule DoctorSchedule.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    alter table(:users) do
       add :email, :string
       add :first_name, :string
       add :last_name, :string
